f9a7afb3ab79a877ee7786529be76b989bb558c7
crickabhi <abhinav@eckovation.com>
2017-05-07 00:36:56 +0530

    Initial Commit

---
+++ KickStarter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KickStarter
+//
+//  Created by Abhinav Mathur on 07/05/17.
+//  Copyright © 2017 Abhinav Mathur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
